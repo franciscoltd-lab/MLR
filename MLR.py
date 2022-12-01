5da32a8cee5b540b513a57b49135d6ce45962e90
Javier Reyna <freyna@blen.com.mx>
2022-12-01 16:15:10 -0600

    MLR terminado

--- MLR.py
+++ MLR.py
@@ -2,40 +2,26 @@ import numpy as np
 import pandas as pd
 
 class MLR:
-    
+
     def __init__(self, dataSet):
 
         self.dataSet = dataSet
-        self.columns = len(dataSet.count())
-        self.n = self.dataSet['Sales'].count()
-
-        self.sumY = sum(dataSet['Sales'])
-        self.sumX1 = sum(dataSet['TV'])
-        self.sumX2 = sum(dataSet['Radio'])
-        self.sumX3 = sum(dataSet['Newspaper'])
-
-        self.sumX1sqr = sum(pow(dataSet['TV'],2))
-        self.sumX2sqr = sum(pow(dataSet['Radio'],2))
-        self.sumX3sqr = sum(pow(dataSet['Newspaper'],2))
-
-        self.sumX1X2 = sum(dataSet['TV'] * dataSet['Radio'] ) 
-        self.sumX1X3 = sum(dataSet['TV'] * dataSet['Newspaper'])
-        self.sumX2X1 = sum(dataSet['Radio'] * dataSet['TV'])
-        self.sumX2X3 = sum(dataSet['Radio'] * dataSet['Newspaper'])
-        self.sumX3X1 = sum(dataSet['Newspaper'] * dataSet['TV'])
-        self.sumX3X2 = sum(dataSet['Newspaper'] * dataSet['Radio'])
-
-        self.sumX1Y = sum(dataSet['TV'] * dataSet['Sales'])
-        self.sumX2Y = sum(dataSet['Radio'] * dataSet['Sales'])
-        self.sumX3Y = sum(dataSet['Newspaper'] * dataSet['Sales'])
-
-        self.data = {'col1': [self.n, self.sumX1, self.sumX2, self.sumX3] , 'col2': [self.sumX1, self.sumX1sqr, self.sumX1X2, self.sumX1X3 ] ,
-                     'col3':[self.sumX2, self.sumX1X2, self.sumX2sqr, self.sumX2X3] , 'col4':[self.sumX3, self.sumX3X1, self.sumX3X2, self.sumX3sqr]}
-
-        self.matrix = pd.DataFrame(data=self.data)
-        self.vector = pd.DataFrame(data={'col':[self.sumY, self.sumX1Y, self.sumX2Y, self.sumX3Y]})
-
-        self.staggeringMatrix = pd.DataFrame(data={'col1':[1,0,0,0],'col2':[0,1,0,0],'col3':[0,0,1,0],'col4':[0,0,0,1]})
+        self.y = self.dataSet['Sales']
+        self.x = self.dataSet[['TV','Radio','Newspaper']]
+        self.x.insert(loc=0, column='Helper', value=1)
         
+        # self.y = self.dataSet['col4']
+        # self.x = self.dataSet[['col1','col2','col3']]
     
+    def leastSquareEstimates(self):
+        x_transposed = self.x.T
 
+        result = np.dot(x_transposed,self.x)
+
+        Inv = np.linalg.inv(result)
+
+        y_trans = np.dot(x_transposed, self.y)
+
+        result = np.dot(Inv, y_trans)
+
+        print(result)
--- MLR_main.py
+++ MLR_main.py
@@ -4,35 +4,13 @@ import pandas as pd
 
 def main():
 
-    # y = pd.read_csv('Advertising.csv', usecols = ['Sales'])
-    # x1 = pd.read_csv('Advertising.csv', usecols = ['TV'])
-    # x2 = pd.read_csv('Advertising.csv', usecols = ['Radio'])
-    # x3 = pd.read_csv('Advertising.csv', usecols = ['Newspaper'])
-    my_absolute_path = r'/home/fltd/Python/pade/examples/MLR/Advertising.csv'
-    dataSet = pd.read_csv(my_absolute_path)
+    # my_absolute_path = r'/home/fltd/Python/pade/examples/MLR/Advertising.csv'
+    # columns=['col1', 'col2', 'col3', 'col4']
+    # dataSet = pd.read_csv('DataSet.txt')
+    dataSet = pd.read_csv('Advertising.csv')
 
     advertising = MLR.MLR(dataSet)
-
-    # print(advertising.n)
-
-    # print(advertising.sumY)
-    # print(advertising.sumX1)
-    # print(advertising.sumX2)
-    # print(advertising.sumX3)
-
-    # print(advertising.sumX1sqr)
-    # print(advertising.sumX2sqr)
-    # print(advertising.sumX3sqr)
-
-    # print('sumX1Y ',advertising.sumX1Y)
-    # print(advertising.sumX2Y)
-    # print(advertising.sumX3Y)
-
-    # print('Columns: ', advertising.columns)
-
-    print(advertising.matrix)
-    print(advertising.vector)
-    print(advertising.staggeringMatrix)
+    advertising.leastSquareEstimates()
 
 
 if __name__ == '__main__':
